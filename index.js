ef93bd256aec1a34b6d6dbeafd1733479e99c481
Kate Schwarz <kateschwarz0010@gmail.com>
2018-03-03 14:06:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,20 +20,20 @@ function divide(a,b)
 
 function inc(n)
 {
-  return n++
+  return ++n
 }
 
 function dec(n)
 {
-  return n--
+  return --n 
 }
 
 function makeInt(n)
 {
-  return parseInt("n",10)
+  return parseInt('n',10)
 }
 
 function preserveDecimal(n)
 {
-  parseFloat("n")
+  return parseFloat('n')
 }
